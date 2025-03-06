4d4ade3fdc1cf3af8f8f8a2631664a85215369f5
Muhammad-Arfan786 <mianarfanaslamgg@gmail.com>
2025-03-06 15:29:56 +0500

    Update Calculator.cs
    Added multiplication functionality

--- Calculator.cs
+++ Calculator.cs
@@ -5,3 +5,13 @@ int main() {
     cout << "Your Name - Roll Number" << endl;
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Product: " << a * b << endl;
+    return 0;
+}
