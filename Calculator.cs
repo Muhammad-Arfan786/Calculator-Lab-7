bff73ea04e04190caea1086c6925f0dafae87f32
Muhammad-Arfan786 <mianarfanaslamgg@gmail.com>
2025-03-06 15:30:49 +0500

    Update Calculator.cs
    Added division functionality

--- Calculator.cs
+++ Calculator.cs
@@ -5,3 +5,17 @@ int main() {
     cout << "Your Name - Roll Number" << endl;
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    if (b != 0) {
+        cout << "Quotient: " << a / b << endl;
+    } else {
+        cout << "Error! Division by zero." << endl;
+    }
+    return 0;
+}
